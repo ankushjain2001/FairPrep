b0427b69b3a9b74baf5ca800937b149e117d3d98
Ankush Jain <ankushjain2001@yahoo.co.in>
2020-04-24 17:45:46 -0500

    Update test_PreCheck.py

--- fp/tests/test_PreCheck.py
+++ fp/tests/test_PreCheck.py
@@ -1,20 +1,26 @@
 import os
+import pickle
 import unittest
+from datetime import datetime
+from freezegun import freeze_time
 
-from fp.traindata_samplers import CompleteData
-from fp.scalers import NoScaler
-from fp.learners import NonTunedLogisticRegression, NonTunedDecisionTree
-from fp.pre_processors import NoPreProcessing
-from fp.post_processors import NoPostProcessing
+from fp.traindata_samplers import CompleteData, BalancedExamplesSampler
+from fp.missingvalue_handlers import CompleteCaseAnalysis, ModeImputer, DataWigSimpleImputer
+from fp.scalers import NoScaler, NamedStandardScaler, NamedMinMaxScaler
+from fp.learners import NonTunedLogisticRegression, NonTunedDecisionTree, DecisionTree, LogisticRegression, AdversarialDebiasing
+from fp.post_processors import NoPostProcessing, RejectOptionPostProcessing, EqualOddsPostProcessing, CalibratedEqualOddsPostProcessing
+from fp.pre_processors import NoPreProcessing, Reweighing, DIRemover, LFR
 from fp.experiments import BinaryClassificationExperiment
 
-class testSummer(unittest.TestCase):
-  # Test 01
-  def testSum01(self):
-    num1 = 4
-    num2 = 10
-    self.assertEqual(14, num1+num2)
-    
+@freeze_time('2020-01-01 00:00:00.000000')
+class test_pre_check(unittest.TestCase):
+    def test_unittest(self):
+        num1 = 3
+        num2 = 10
+        self.assertEqual(num1+num2, 13)
+
+    def test_freezegun(self):
+        self.assertEqual(datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')[:-3], '2020-01-01_00-00-00-000')
+        
 if __name__ == '__main__':
     unittest.main()
-    
